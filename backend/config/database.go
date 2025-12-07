46369ed43738b863753f1a714f220565ad8eebcc
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-12-07 16:29:18 +0700

    god help us

--- backend/config/database.go
+++ backend/config/database.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -9,15 +11,37 @@ import (
 
 var DB *gorm.DB
 
-// MySQL connection - hardcoded for local development
-// Database: smartgement, User: root, No password
-const DSN = "root:@tcp(127.0.0.1:3306)/smartgement?charset=utf8mb4&parseTime=True&loc=Local"
-
 func InitDatabase() {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	// Provide default values if environment variables are not set
+	if dbUser == "" {
+		dbUser = "root"
+	}
+	if dbPass == "" {
+		dbPass = "" // No password by default
+	}
+	if dbHost == "" {
+		dbHost = "127.0.0.1"
+	}
+	if dbPort == "" {
+		dbPort = "3306"
+	}
+	if dbName == "" {
+		dbName = "smartgement"
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser, dbPass, dbHost, dbPort, dbName)
+
 	var err error
-	DB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to MySQL database:", err)
+		log.Fatalf("Failed to connect to MySQL database: %v", err)
 	}
 
 	log.Println("MySQL database connected successfully!")
